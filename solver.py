d34fbc26f97e3c9543faddee21553b945853790b
Praneeth Kanna <praneeth.kanna@berkeley.edu>
2021-05-03 23:15:42 -0400

    added changes

--- solver.py
+++ solver.py
@@ -4,8 +4,49 @@ from utils import is_valid_solution, calculate_score
 import sys
 from os.path import basename, normpath
 import glob
+import matplotlib.pyplot as plt
 import itertools as it
 
+
+def getEdgesInClique(clique, G):
+    edges = []
+    possibleEdges = list(it.combinations(clique, 2))
+    for edge in possibleEdges:
+        if edge in G.edges:
+            edges.append(edge)
+    return edges
+
+def getMostImpactfulNode(G):
+    endNode = len(G.nodes) - 1
+    bestNode = (None, -1)
+    for node in G.nodes:
+        if node == 0 or node == endNode:
+            continue
+        newG = G.copy()
+        newG.remove_node(node)
+        if not nx.is_connected(newG):
+            continue
+        dists, paths = nx.single_source_dijkstra(newG, 0)
+        if dists[endNode] > bestNode[1]:
+            bestNode = (node, dists[endNode])
+    return bestNode[0]
+
+def getMostImpactfulEdge(combos, G):
+    endNode = len(G.nodes) - 1
+    bestEdge = (None, -1)
+    # print(combos)
+    for edges in combos:
+        newG = G.copy()
+        for edge in edges:
+            if newG.has_edge(*edge):
+                newG.remove_edge(edge[0], edge[1])
+            if not nx.is_connected(newG):
+                newG.add_edge(edge[0], edge[1])
+                continue
+        dists, paths = nx.single_source_dijkstra(newG, 0)
+        if dists[endNode] > bestEdge[1]:
+            bestEdge = (edges, dists[endNode])
+    return bestEdge[0]
 
 def solve(G):
     """
@@ -21,37 +62,103 @@ def solve(G):
     c = 1
     endNode = len(G.nodes) - 1
     newG = G.copy()
-    print(G.nodes)
+    edgeToKeep = []
 
-    bestNode = (None, -1)
-    for node in list(G.nodes):
-        if node == 0 or node == endNode:
-            continue
-        Gprime = newG.copy()
-        Gprime.remove_node(node)
-        dists, paths = nx.single_source_dijkstra(Gprime, 0)
-        if dists[endNode] > bestNode[1]:
-            bestNode = (node, dists[endNode])        
-    cities.append(bestNode[0])
+    for _ in range(c):
+        bestNode = getMostImpactfulNode(newG)     
+        cities.append(bestNode)
+        newG.remove_node(bestNode)
+
+    while k > 0:
+        assert nx.is_connected(newG)
+        dists, paths = nx.single_source_dijkstra(newG, 0)
+        shortestPath = paths[endNode]
+        edgeList = []
+        # for i in range(len(shortestPath) - 1):
+        #     edgeList.append((shortestPath[i], shortestPath[i+1]))
+        for node in shortestPath:
+            for edge in newG.edges:
+                if edge[0] == node or edge[1] == node:
+                    edgeList.append(edge)
+        edgeList = list(set([i for i in edgeList]))
+        for edge in edgeToKeep:
+            if edge in edgeList:
+                edgeList.remove(edge)
+        print(edgeList)
+        edgeCombinations = list(it.combinations(edgeList, 2))
+        print("Number of combos: ", len(edgeCombinations))
+
+        bestCombination = getMostImpactfulEdge(edgeCombinations, newG)
+        print('best: ', bestCombination)
+        if bestCombination:
+            for edge in bestCombination:
+                if k == 0:
+                    break
+                if newG.has_edge(*edge):
+                    k -=1
+                    edges.append(edge)
+                    newG.remove_edge(edge[0], edge[1])
+                if not nx.is_connected(newG):
+                    # k += 1
+                    edges.remove(edge)
+                    newG.add_edge(edge[0], edge[1])
+                    edgeToKeep.append(edge)
+
+
+    print('Cities: ', cities)
+    print('Edges: ', edges)
+    return cities, edges
+
+def solve2(G):
+    """
+    Args:
+        G: networkx.Graph
+    Returns:
+        c: list of cities to remove
+        k: list of edges to remove
+    """
+    cities = []
+    edges = []
+    k = 15
+    c = 1
+    endNode = len(G.nodes) - 1
+    newG = G.copy()
+
+    for _ in range(c):
+        bestNode = getMostImpactfulNode(newG)     
+        cities.append(bestNode)
+        newG.remove_node(bestNode)
 
     for _ in range(k):
         assert nx.is_connected(newG)
         dists, paths = nx.single_source_dijkstra(newG, 0)
         shortestPath = paths[endNode]
-        shortestEdge = (None, float('inf'))
+        edgeList = []
         for i in range(len(shortestPath) - 1):
-            edge = G.edges[shortestPath[i], shortestPath[i+1]]
-            if edge['weight'] < shortestEdge[1]:
-                testG = newG.copy()
-                testG.remove_edge(shortestPath[i], shortestPath[i+1])
-                if not nx.is_connected(testG):
-                    continue
-                shortestEdge = ((shortestPath[i], shortestPath[i+1]), edge['weight'])
-        # print(shortestPath)
-        # print(shortestEdge)
-        edges.append(shortestEdge[0])
-        newG.remove_edge(shortestEdge[0][0], shortestEdge[0][1])
+            edgeList.append((shortestPath[i], shortestPath[i+1]))
+        print(edgeList)
+        edgeCombinations = list(it.combinations(edgeList, 2))
+        print("Number of combos: ", len(edgeCombinations))
 
+        bestCombination = getMostImpactfulEdge(edgeCombinations, newG)
+        print('best: ', bestCombination)
+        if bestCombination:
+            for edge in bestCombination:
+                if k == 0:
+                    break
+                if newG.has_edge(*edge):
+                    k -=1
+                    edges.append(edge)
+                    newG.remove_edge(edge[0], edge[1])
+                if not nx.is_connected(newG):
+                    # k += 1
+                    edges.remove(edge)
+                    newG.add_edge(edge[0], edge[1])
+                    edgeToKeep.append(edge)
+
+
+    print('Cities: ', cities)
+    print('Edges: ', edges)
     return cities, edges
 
 
@@ -59,20 +166,29 @@ def solve(G):
 
 # Usage: python3 solver.py test.in
 
+def single_file(lol):
+    path = 'inputs/small/small-' + str(lol) +  '.in'
+    # path = 'test.in'
+    # path = 'inputs/medium/medium-' + str(lol) +  '.in'
+    # path = 'inputs/large/large-' + str(lol) +  '.in'
+    G = read_input_file(path)
+    # c, k = solve2(G)
+    c, k = solve(G)
+    assert is_valid_solution(G, c, k)
+    print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
+    write_output_file(G, c, k, 'small-test.out')
+
 if __name__ == '__main__':
-    # assert len(sys.argv) == 2
-    # path = sys.argv[1]
-    for i in range(1, 100):
+    # single_file(2)
+    for i in range(21, 301):
         print("INPUT: ", i)
         path = 'inputs/small/small-' + str(i) + '.in'
         G = read_input_file(path)
-        c, k = solve(G)
+        c, k = solve2(G)
         assert is_valid_solution(G, c, k)
         print("Shortest Path Difference: {}".format(calculate_score(G, c, k)))
         output_path = 'outputs/small-' + str(i) + '.out'
         write_output_file(G, c, k, output_path)
-    # write_output_file(G, c, k, 'outputs/small-1.out')
-
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 # if __name__ == '__main__':
